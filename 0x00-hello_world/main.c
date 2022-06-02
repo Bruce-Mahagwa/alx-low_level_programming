09967556b09e6a48d1791622971ccbe826fd9d58
Bruce <jacobbruce880@gmail.com>
2022-06-02 07:33:19 -0700

    Compile but do not link

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Prints Hello World
+ *
+ * Return: Returns 0
+ */
 int main(void)
 {
-printf("Hello world\n");
-return 0;
+	printf("Hello world\n");
+	return (0);
 }
