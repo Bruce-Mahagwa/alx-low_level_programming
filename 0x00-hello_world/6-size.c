56c32e1168cc08b25aa00bde4632b06a34432734
Bruce <jacobbruce880@gmail.com>
2022-06-02 06:55:04 -0700

    Get sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int a = 16;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
