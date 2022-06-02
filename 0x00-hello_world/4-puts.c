32d728da54d50cb4d9ff15d629c984e0fe2373eb
Bruce <jacobbruce880@gmail.com>
2022-06-02 05:03:24 -0700

    Using put to print a line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+int main(void){
+char line="Programming is like building a multilingual puzzle";
+puts(line);
+return 0;
+}
