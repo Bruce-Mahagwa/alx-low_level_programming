7b228b8579a5495b1ad06b390beed7bd8cbf61fc
Bruce <jacobbruce880@gmail.com>
2022-06-02 05:26:02 -0700

    Print a line with printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char line[] = \"Programming is like building a multilingual puzzle";
+	char line[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(line);
 	return 0;
