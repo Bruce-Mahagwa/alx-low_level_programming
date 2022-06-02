0b7b70604f62c4acda3975ba410dceb61e0f23bd
Bruce <jacobbruce880@gmail.com>
2022-06-02 05:40:45 -0700

    Print a line with printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - Prints a line using printf
- *
-(* Description: This function prints a single line using printf)?
-
- * Return 0
+(*
+ * Description: This function prints a single line using printf)?
+ * Return: The function returns 0
  */
 int main(void)
 {
