2722185e34f546cdc467475b5f9a5b722c437a8d
Bruce <jacobbruce880@gmail.com>
2022-06-02 05:24:27 -0700

    Printing a line with printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * Prints a line using printf
+ * Return 0
  */
 int main(void)
 {
-	char line[]="Programming is like building a multilingual puzzle";
+	char line[] = \"Programming is like building a multilingual puzzle";
+
 	puts(line);
 	return 0;
 }
