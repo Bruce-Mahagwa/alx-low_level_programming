999041fde1503478f600cc40797f2574c45464ed
Bruce <jacobbruce880@gmail.com>
2022-06-02 05:43:34 -0700

    Print a line with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Prints with printf
+ * Return: The function returns zero
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art");
-	return 0;
+	return (0);
 }
