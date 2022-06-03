bdfffd767e480467ca0510e24257111b3e9cf705
Bruce <jacobbruce880@gmail.com>
2022-06-03 02:04:09 -0700

    Print numbers with a newline

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/** 
+ * main - Print even numbers
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i ++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints single dgit numbers 
+ *
+ * Return: Returns zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i ++)
+	{
+		putchar(i\n);
+	}
+	return (0);
+}
