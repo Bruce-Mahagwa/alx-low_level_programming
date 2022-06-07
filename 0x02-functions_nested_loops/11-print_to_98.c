fb839ffa32936dfc5116ba93070e4ddc7112e369
Bruce <jacobbruce880@gmail.com>
2022-06-07 09:53:47 -0700

    Print numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,8 +8,6 @@
  */
 void print_to_98(int n)
 {
-	int y = 98;
-
 	if (n == 98)
 	{
 		printf("%d\n", n);
@@ -21,14 +19,16 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
+		printf("%d", 98);
 	}
-	if (n == 98)
+	if (n > 98)
 	{
 		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+		printf("%d", 98);
 	}
 	
 }
