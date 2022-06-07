19e667fa732cc5322514bec96e213d3bf36dabea
Bruce <jacobbruce880@gmail.com>
2022-06-07 10:14:30 -0700

    Prints the sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		return (1);
+		putchar(1);
 	}
 	else if (n < 0)
 	{
 		putchar('-');
-		return (-1);
+		putchar(1);
 	}
 	else {
 		putchar(0);
-		return (0);
+		putchar(0);
 	}
 	return (0);
 }
