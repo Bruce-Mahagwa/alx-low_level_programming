1a634b9e874b5ce555bbda54b3c6bae0a55fdda5
Bruce <jacobbruce880@gmail.com>
2022-06-07 08:35:02 -0700

    Print timestable

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - Prints timein 24 hours
+ *
+ * Return: Returns time
+ */
+void jack_bauer(void)
+{
+	float i;
+
+	for (let i = 00.00; i <= 23:59; i ++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**times_table - Prints the 9 timestable
+ *
+ * Returns: Returns 0
+ */
+void times_table(void)
+{
+	int x = 0;
+
+	int y;
+
+	while (x < 10)
+	{
+		y = 0;
+		while (y <= 81)
+		{
+			putchar(y);
+			y + 9;
+		}
+		putchar('\n');
+		x++;
+	}
+}
