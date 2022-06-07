5d103382420050ebd51b7e71a6035f29f505e4fe
Bruce <jacobbruce880@gmail.com>
2022-06-07 09:21:43 -0700

    Print numbers in a range

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,21 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	
 }
