dfe4a7d399d574ba570b48b09a344ac5e32313dd
Bruce <jacobbruce880@gmail.com>
2022-06-07 04:02:43 -0700

    Uses isalpha to check for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - checks for alpha characters only
+ *
+ * Return: Returns 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c) == 0)
+	{
+		return(0);
+	}
+	else if (isalpha(c) != 0)
+	{
+		return(1);
+	}
+}
