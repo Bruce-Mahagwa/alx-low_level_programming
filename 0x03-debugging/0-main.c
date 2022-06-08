3713e0e1043649ed1c85725bb5cd2bd8e32258ba
Bruce <jacobbruce880@gmail.com>
2022-06-08 02:14:35 -0700

    Tests for the sign of a number

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * positive_or_negative - tests the sign of an int
+ * @i: First operand
+ *
+ * Return: Returns nothing
+ */
+void positive_or_negative(int 0);
