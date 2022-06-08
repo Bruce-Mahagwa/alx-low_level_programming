7f6971bff1d7d06f35c5abbce5fcb103aad3fcb2
Bruce <jacobbruce880@gmail.com>
2022-06-08 02:39:53 -0700

    Stop an infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - causes an infinite loop
+ *
+ * Return: Returns zero
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+/*	i = 0;
+
+	while (i < 10)
+{
+	putchar(i);
+}*/
+	printf("Infinite loop avoided! \\o/\n");
+
+	return(0);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,3 +6,4 @@
 void positive_or_negative(int i);
 
 #endif /*MAIN_H*/
+int main(void);
