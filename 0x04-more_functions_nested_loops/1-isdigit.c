8aaa0bf29a2f2f0b41c542a3fcaaf5aa4556a0c4
Bruce <jacobbruce880@gmail.com>
2022-06-09 07:22:27 -0700

    Check if a char is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,13 +9,17 @@
  */
 int _isdigit(int c)
 {
-	c = '6';
-	if (isdigit(c))
+	if (c >= 0 && c <= 9)
 	{
-		return (1);
-	}
-	else {
-		return (0);
-	}
+
+		if (isdigit(c))
+		{
+			return (1);
+		}
+		else 
+		{
+			return (0);
+		}
 	return (0);
+	}
 }
