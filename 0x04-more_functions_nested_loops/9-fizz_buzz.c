23b578e76f29c6f22746c1f098741b971aa77ca4
Bruce <jacobbruce880@gmail.com>
2022-06-09 04:32:40 -0700

    Prints 1 to 100 with interpolations of fz and buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * fizz_buzz - Prints numbers 1 to 100 with fizz and buzz interpolations
+ *
+ * Return: Retuns zero
+ */
+int fizz_buzz()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	putchar(' ');
+	putchar('\n');
+	}
+	return (0);
+}
