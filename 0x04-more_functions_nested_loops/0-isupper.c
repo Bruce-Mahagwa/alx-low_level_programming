6651b40ba45464d8b32338596c3f96a7ef7cb812
Bruce <jacobbruce880@gmail.com>
2022-06-09 01:10:10 -0700

    Check if character is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks if a letter is uppercase
+ *
+ * @c: First operand
+ *
+ * Return: Returns either one or 0
+ */
+int _isupper(int c)
+{
+	c = 'A';
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
