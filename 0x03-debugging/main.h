03f9c3ed1afa2b178d3b76db6d5907bfbb0487d4
Bruce <jacobbruce880@gmail.com>
2022-06-09 06:08:43 -0700

    Tests a function that determines the sign of a number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
-
 void positive_or_negative(int i);
-
-#endif /*MAIN_H*/
 int main(void);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
