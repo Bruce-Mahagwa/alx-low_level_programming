4742bc550f368493382d561b869be8ce22d1d59c
Bruce <jacobbruce880@gmail.com>
2022-06-13 03:18:06 -0700

    Finds the string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,10 +4,12 @@
 /**
  *_strlen - finds the length of a string
  * @s: First operand
- * Return: Returns zero
+ * Return: Returns string length
  */
 int _strlen(char *s)
 {
-	strlen(s);
-	return (0);
+	int n;
+
+	n = strlen(s);
+	return (n);
 }
