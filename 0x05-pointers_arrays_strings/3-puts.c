77777c3d042eed57484fd2f2f94b0a330c3ba136
Bruce <jacobbruce880@gmail.com>
2022-06-13 03:39:46 -0700

    Prints to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - prints a string to stdoutput
+ * @str: First operand
+ *
+ * Return: Returns void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
