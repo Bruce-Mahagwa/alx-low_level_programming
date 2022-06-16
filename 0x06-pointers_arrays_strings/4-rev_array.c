424baad71fa8af32ab26f22fb2f727b02e44e8f9
Bruce <jacobbruce880@gmail.com>
2022-06-16 09:07:21 -0700

    Reverses an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,14 +14,14 @@ void reverse_array(int *a, int n)
 
 	int t;
 
+	int j;
+
 	for (i = 0; i < n / 2; i++)
 	{
 		t = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = t;
 	}
-	int j;
-
 	for (j = 0; j < n; j++)
 	{
 		_putchar(a[j]);
