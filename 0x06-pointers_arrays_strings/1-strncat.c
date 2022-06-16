f262bde376d3dd2e5cdfa3d207fccf8e6e3fdb54
Bruce <jacobbruce880@gmail.com>
2022-06-16 04:43:47 -0700

    Concatenates 2 strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,5 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-
-	int j;
-
-	int k;
-
-	i = strlen(dest);
-	j = strlen(src);
-	k = i + j;
-	char strings[k];
-
-	strcpy(strings, dest);
-	return (strncat(strings, src, n));
+	return (strncat(dest, src, n));
 }
