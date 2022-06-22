11b7f95a1c796dc370bde90497be127b10f0cad2
Bruce <jacobbruce880@gmail.com>
2022-06-22 04:33:54 -0700

    Finds factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,9 +9,17 @@
  */
 int factorial(int n)
 {
-	if (n <= 1)
+	if (n = 1)
 	{
 		return (1);
+	}
+	if (n = 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
 	}
 	return (n * factorial(n - 1));
 }
