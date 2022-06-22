a7490ec567bb4b179db05f1de9424a98a3fd2e40
Bruce <jacobbruce880@gmail.com>
2022-06-22 05:18:52 -0700

    Raises a number to a power

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+/**
+ * _sqrt_recursion - Finds the square root of a number
+ * @n: First operand
+ *
+ * Return: Returns the square root
+ */
+int _sqrt_recursion(int n)
+{
+
