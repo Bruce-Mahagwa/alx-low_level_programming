6b68a67258d55f8c97b8565576e01001bcb93eea
Bruce <jacobbruce880@gmail.com>
2022-06-22 04:42:16 -0700

    Raises a number to a power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+/**
+ * _pow_recursion - Finds the power of a number
+ * @x: Number to be raised
+ * @y: Power
+ *
+ * Return: Returns the result
+ */
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
