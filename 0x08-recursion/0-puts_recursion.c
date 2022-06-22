3d8cc497e0df10c1ff2622382019633293f58769
Bruce <jacobbruce880@gmail.com>
2022-06-22 02:53:18 -0700

    Puts a string to std output

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _puts_recursion - puts a string to standard output
+ * @s: First operand
+ *
+ * Return: Void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
