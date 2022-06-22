fb4eb826833c7039821ae4a86380de96814f610a
Bruce <jacobbruce880@gmail.com>
2022-06-22 04:13:01 -0700

    Finds string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen_recursion - prints length of a string
+ * @s: First operand
+ *
+ * Return: Returns length of string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,3 +2,4 @@ void _puts_recursion(char *s);
 int main(void);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
