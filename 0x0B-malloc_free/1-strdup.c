b353cd874f90f9ea0981258065e9ecca3542f1f3
Bruce <jacobbruce880@gmail.com>
2022-06-27 09:59:34 -0700

    Copies a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - Copies a string
+ * @s: String to be copied
+ *
+ * Return: A pointer to the string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+
+	int i = 0;
+
+	int sum = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		sum += i;
+	}
+	s = malloc(sizeof(char) * sum);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,1 +1,3 @@
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
+char *_strdup(char *str);
