ac392b40846d4ad55ffaf5cf95635698509529e9
Bruce <jacobbruce880@gmail.com>
2022-06-29 03:14:57 -0700

    Creates a grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,38 @@
 int **alloc_grid(int width, int height)
 {
 	int **s;
-	
-	int i, j, k;
+
+	int i, j, k, y;
 
 	s = malloc((width * height) * sizeof(int));
-	if (width <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	if (height <= 0)
-	{
-		return (NULL);
-	}
+	s = (int **)malloc(sizeof(int *) * height);
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < (width * height); i++)
+	for (i = 0; i < height; i++)
 	{
-		s[i] = 0;
-	}
-	for (j = 0; j < width; j++)
-	{
-		for (k = 0; k < height; k++)
+
+		s[i] = (int *)malloc(sizeof(int) * width);
+		if (s[i] == NULL)
 		{
-			*s[i] = j * height + k;
+			free(s);
+			for (y = 0; y <= i; y++)
+			{
+				free(s[y]);
+			}
+		return (NULL);
+		}
+	}
+	for (j = 0; j < height; j++)
+	{
+		for (k = 0; k < width; k++)
+		{
+			s[j][k] = 0;
 		}
 	}
 	return (s);
