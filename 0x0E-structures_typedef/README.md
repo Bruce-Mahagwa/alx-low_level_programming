041a8b6b3995aa74d2c853678eb45c85c49b992f
Bruce <jacobbruce880@gmail.com>
2022-07-04 06:40:09 -0700

    Creates a struct named dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Learning about structures and typedefs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * struct dog - Creates a struct named dog
+ * @name: Dog's name a char pointer
+ * @age: Dog's age a float
+ * @owner: Dog's owner a char pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
