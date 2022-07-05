31b22761a691c0277857c83ef59e28fa4057ae04
Bruce <jacobbruce880@gmail.com>
2022-07-05 02:13:52 -0700

    Prints a dog struct properties

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints elementsof a struct
+ * @d: a dog struct
+ *
+ * Return: Returns void
+ */
 void print_dog(struct dog *d)
 {
-	struct dog *p = d;
-	p->
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: nil\n");
+	}
+	else if (isdigit(d->age) == 0)
+	{
+		printf("Age: nil\n");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Owner: nil\n");
+	}
+	else
+	{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+	}
 }
