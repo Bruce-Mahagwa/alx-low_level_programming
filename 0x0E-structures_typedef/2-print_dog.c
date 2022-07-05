c1ea0d741151ef9c5bc7b672fb2bac11e140d0a8
Bruce <jacobbruce880@gmail.com>
2022-07-05 02:20:31 -0700

    Prints props of a dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,18 +20,24 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: nil\n");
 	}
-	else if (isdigit(d->age) == 0)
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (isdigit(d->age) == 0)
 	{
 		printf("Age: nil\n");
 	}
-	else if (d->owner == NULL)
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
 	{
 		printf("Owner: nil\n");
 	}
 	else
 	{
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 }
