2eec1b23e8652e8297c39c2f8134863c8e30e031
Bruce <jacobbruce880@gmail.com>
2022-07-05 01:41:25 -0700

    Creates a new struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,7 +15,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL)
 	{
 		d = malloc(sizeof(struct dog));
-		if (d == NULL)									return;									}
+		if (d == NULL)
+			return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
