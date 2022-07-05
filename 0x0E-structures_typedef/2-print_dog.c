de0b0d1b61815d779d73707de321617c5b2b4ce8
Bruce <jacobbruce880@gmail.com>
2022-07-05 02:34:14 -0700

    Fixes compile issues

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,13 +23,6 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("Name: %s\n", d->name);
-	}
-	if (isdigit(d->age) == 0)
-	{
-		printf("Age: nil\n");
-	}
-	else
-	{
 		printf("Age: %f\n", d->age);
 	}
 	if (d->owner == NULL)
