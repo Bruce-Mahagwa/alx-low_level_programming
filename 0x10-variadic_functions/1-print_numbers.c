3c608ef7bf18cf0ee812bcc002cfcda7ccf6fe5e
Bruce <jacobbruce880@gmail.com>
2022-07-07 02:54:47 -0700

    Prints numbers with a separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - lists number args
+ * @separator: a comma
+ * @n: size of args list
+ *
+ * Return: Returns void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+
+	unsigned int i;
+
+	va_start(list, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%u ", va_arg(list, int));
+		}
+		else
+		{
+			printf("%u%s", va_arg(list, int), separator);
+		}
+	}
+	for (i = (n - 1); i < n; i++)
+	{
+		printf("%u", va_arg(list, int));
+	}
+	printf("\n");
+	va_end(list);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,3 +2,4 @@
 #define VARIADIC_FUNCTIONS_H
 #endif
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
