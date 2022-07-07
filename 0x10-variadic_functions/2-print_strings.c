8b870a484b46ac53b5ede53dca7d82827fbcf640
Bruce <jacobbruce880@gmail.com>
2022-07-07 04:33:45 -0700

    j

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,14 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		if (separator[0] == '\0')
-		{
-			printf("%s ", va_arg(list, char *));
-		}
-		else
-		{
-			printf("%s%s", va_arg(list, char *), separator);
-		}
+		printf("%s%s ", va_arg(list, char *), separator);
 	}
 	for (j = n - 1; j < n; j++)
 	{
