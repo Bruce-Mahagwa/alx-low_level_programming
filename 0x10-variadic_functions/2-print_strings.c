71a9f45d510ec4ed5a2afd4aedefd6d02a6dca4e
Bruce <jacobbruce880@gmail.com>
2022-07-07 04:01:35 -0700

    Fix compile issues

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints a string
+ * @separator: goes between the strings
+ * @n: number of args
+ *
+ * Return: Returns void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+
+	unsigned int i, j;
+
+	va_start(list, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%s ", va_arg(list, char *));
+		}
+		else
+		{
+			printf("%s%s ", va_arg(list, char *), separator);
+		}
+	}
+	for (j = n - 1; j < n; j++)
+	{
+		printf("%s", va_arg(list, char *));
+	}
+	printf("\n");
+	va_end(list);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-#endif
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif
