8b96db2456557e961de39c85f70a0cf3d7d66642
Bruce <jacobbruce880@gmail.com>
2022-07-14 04:33:34 -0700

    trial

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**len - finds length of string
+ * @: a string
+ *
+ * Return: Returns length
+ */
+int len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+/**
+ * add_node - adds a node at the front
+ * @head: reference to the head of a list
+ * @str: data to be inserted
+ *
+ * Return: Address of new element or null
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str ==NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = len(strdup(str));
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * list_t - an alias for a struct
  * @str: a string pointer
@@ -16,4 +18,5 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
