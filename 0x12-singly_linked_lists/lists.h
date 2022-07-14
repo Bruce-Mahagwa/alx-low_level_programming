e3c310f036f7844c6219c71c23d6e8f410e60e26
Bruce <jacobbruce880@gmail.com>
2022-07-14 03:04:20 -0700

    Adding putchar prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <unistd.h>
 /**
  *
  *  * struct list_s - singly linked list
@@ -27,5 +28,26 @@ typedef struct list_s
 		    struct list_s *next;
 
 } list_t;
+/**
+ *
+ *  * _putchar - writes the character c to stdout
+ *
+ *   * @c: The character to print
+ *
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *
+ *       */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
 size_t print_list(const list_t *h);
 #endif
