1ab0196a4b22aa7cdb7f36c4f0a820f326a37beb
Bruce <jacobbruce880@gmail.com>
2022-07-21 04:57:18 -0700

    Replaces bit with 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * clear_bit - sets value of a bit to 0
+ * @n: pointer to a decimal
+ * @index: index to be set
+ *
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int c;
+
+	if (index > sizeof(char) * 8 -1)
+	{
+		return (-1);
+	}
+	c = ~(1 << index);
+	*n = *n & c;
+	return (1);
+}
