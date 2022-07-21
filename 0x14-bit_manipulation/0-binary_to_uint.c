04b22ce0c66a8bb89ffdf2518723b7d0e908adf7
Bruce <jacobbruce880@gmail.com>
2022-07-21 02:40:07 -0700

    Converts binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an int
+ * @b: pointer to a binary
+ *
+ * Return: The number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int base, num, mod;
+
+	unsigned int i, j;
+
+	i = 0;
+	j = 0;
+	base = 1;
+	num = 0;
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[i] && (b[i] >= '0' && b[i] <= '1'))
+	{
+		j = j * 10 + (b[i] - '0');
+		i++;
+	}
+	while (j > 0)
+	{
+		mod = j % 10;
+		num = num + (mod * base);
+		j = j / 10;
+		base = base * 2;
+	}
+	return (num);
+}
+
