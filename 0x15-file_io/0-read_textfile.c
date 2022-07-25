d386960f78b6fbc8ec27f2487bb396fe3c9b57a4
Bruce <jacobbruce880@gmail.com>
2022-07-25 01:46:55 -0700

    Reads a poem file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * read_textfile - reads and writes a file
+ * @filename: file to be created
+ * @letters: number of letters to be printed
+ *
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+
+	char *buf;
+
+	ssize_t i, count;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	count = read(fd, buf, letters);
+	close(fd);
+	if (count == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	i = write(STDOUT_FILENO, buf, count);
+	free(buf);
+	if (i != count)
+	{
+		return (0);
+	}
+	return (i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Learning about file I/O
